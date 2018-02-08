ca1da09a0a4e6378ebcb343d4600a80f73855e84
Brian Connor <thaglove10@gmail.com>
2018-02-08 11:36:06 -0500

    prepared connect-sql for offline dev

--- sql-connect.php
+++ sql-connect.php
@@ -1,8 +1,8 @@
 <?php
 
-DEFINE ('DB_USER', 'gameripp');
-DEFINE ('DB_PSWD', 'G0der!ch');
-DEFINE ('DB_HOST', '198.136.49.194');
+DEFINE ('DB_USER', 'gameripp_tarfuin');
+DEFINE ('DB_PSWD', 'ZlXEWnq8tU7C2xTh');
+DEFINE ('DB_HOST', 'localhost');
 DEFINE ('DB_NAME', 'gameripp_master');
 
 $dbcon = mysqli_connect(DB_HOST, DB_USER, DB_PSWD, DB_NAME);
