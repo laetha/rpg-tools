06686d7b50446d682fcb063022bce154bf50b31b
Brian Connor <thaglove10@gmail.com>
2018-02-09 11:33:08 -0500

    Messing with auto-linking. Not working yet.

--- tools/compendium/ForgeForge.php
+++ tools/compendium/ForgeForge.php
@@ -8,8 +8,10 @@
 	<body>
 <?php
 	include('../../sql-connect.php');
-	$sqlcompendium = "SELECT * FROM `compendium` WHERE `title` LIKE 'forgeforge'";
+	$sqlcompendium = "SELECT * FROM compendium";
 	$compendiumdata = mysqli_query($dbcon, $sqlcompendium) or die('error getting data');
+	$sqlget = "SELECT * FROM npcs";
+	$sqldata = mysqli_query($dbcon, $sqlget) or die('error getting data');
 	?>
 		<div class="container">
 			<div class="mainbox">
@@ -17,20 +19,28 @@
 				<div class="menu">
 					<p class ="bodytext" id="demo">
 						<?php
-							$sqlcompendium = "SELECT * FROM `compendium` WHERE `title` LIKE 'forgeforge'";
-							$compendiumdata = mysqli_query($dbcon, $sqlcompendium) or die('error getting data');
-							while($row =  mysqli_fetch_array($compendiumdata, MYSQLI_ASSOC)) {
+							$compendiumtitle = "SELECT * FROM `compendium` WHERE `title` LIKE 'forgeforge'";
+							$titledata = mysqli_query($dbcon, $compendiumtitle) or die('error getting data');
+							while($row =  mysqli_fetch_array($titledata, MYSQLI_ASSOC)) {
 								echo $row['body'];
 							}
 						?>
 						</p>
 						<script>
-						window.onload = myFunction("ForgeForge")
+						<?php
+							while($row = mysqli_fetch_array($compendiumdata, MYSQLI_ASSOC)) {
+								echo "TEST"
+						?>
+								window.onload = myFunction("<?php echo $row['title'] ?>")
+						<?php
+							}
+						?>
+						//window.onload = myFunction("ForgeForge")
 					function myFunction(querytext) {
     			var bodytext = document.getElementById("demo").innerHTML;
     			var n = bodytext.search(querytext);
 					if (n !== -1){
-						var newtext = bodytext.replace("ForgeForge", "<a href=\"onenote:https://d.docs.live.net/acd661a65c5fe18b/Documents/Homebrew%20Campaign/Groups%20and%20Organizations.one#ForgeForge\">ForgeForge</a>")
+						var newtext = bodytext.replace("<?php echo $row['title'] ?>", "<a href=\"onenote:https://d.docs.live.net/acd661a65c5fe18b/Documents/Homebrew%20Campaign/Groups%20and%20Organizations.one#ForgeForge\"><?php echo $row['title'] ?></a>")
 					}
 					else{
 						newtext = bodytext;
@@ -40,8 +50,6 @@
 					</script>
 				</div>
 				<?php
-					$sqlget = "SELECT * FROM npcs";
-					$sqldata = mysqli_query($dbcon, $sqlget) or die('error getting data');
 					echo '<table>';
 					echo '<tr><th>ID</th><th>Name</th><th>Race</th><th>Job</th></tr>';
 					while($row = mysqli_fetch_array($sqldata, MYSQLI_ASSOC)) {
