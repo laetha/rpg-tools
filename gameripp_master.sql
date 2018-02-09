515daa5139bee21bbb1bb9756585a850772c5acd
Brian Connor <thaglove10@gmail.com>
2018-02-09 11:34:07 -0500

    updated sql file

--- gameripp_master.sql
+++ gameripp_master.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.3
+-- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Feb 08, 2018 at 11:12 AM
--- Server version: 5.5.58-cll
--- PHP Version: 5.6.30
+-- Host: 127.0.0.1
+-- Generation Time: Feb 09, 2018 at 05:33 PM
+-- Server version: 10.1.30-MariaDB
+-- PHP Version: 7.2.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -32,8 +32,18 @@ CREATE TABLE `compendium` (
   `id` int(11) NOT NULL,
   `title` text NOT NULL,
   `type` text NOT NULL,
-  `body` text NOT NULL
+  `body` text NOT NULL,
+  `image` text NOT NULL,
+  `url` text NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `compendium`
+--
+
+INSERT INTO `compendium` (`id`, `title`, `type`, `body`, `image`, `url`) VALUES
+(0, 'ForgeForge', 'faction', 'Created in worship of a great Giant of old, ForgeForge is a group primarily focused on fantastic craftsmanship in weapons and armor.  While ForgeForge  members will sell many of their wares for coin, it is much more common for the most exquisitely done pieces to be traded among members, or gifted in an ultimate sign of respect and gratitude.\r\n\r\nThey\'re a very well respected organization that almost entirely operates above board and whose hierarchy  is based exclusively on the skill of one\'s craftsmanship. Also, here\'s the word Westfair.', '', 'forgeforge.php'),
+(1, 'Westfair', 'settlement', 'Hope\'s Landing was a town of a location and style that suited much of the human settlers very well, however they sought a place to call their home. After travelling along the coast they happened upon a perfect location to set up their new home, Westfair. Westfair is a coastal town with fairly ready access to resources via the sea and the nearby Shadewood.\r\n\r\nIts location in a diverse and very wild ecosystem made it the primary location for  The Crimson Seal , who make their headquarters in The Crimson Lodge within Westfair. The Lodge provides much of the identity of the town, as much of the economy in the town revolves around the hunt and its side businesses.', '', 'westfair.php');
 
 -- --------------------------------------------------------
 
