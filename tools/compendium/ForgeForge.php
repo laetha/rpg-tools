b7d999e4ce34751f2aba4d9a4f62f7dc6204bb37
Brian Connor <thaglove10@gmail.com>
2018-02-10 16:22:13 -0500

    edited CSS and cleaned up text padding.

--- tools/compendium/ForgeForge.php
+++ tools/compendium/ForgeForge.php
@@ -15,7 +15,7 @@
 			<div class="mainbox">
 				<h1 class="pagetitle">ForgeForge</h1>
 				<div class="menu">
-					<p class ="bodytext" id="demo">
+					<p class ="bodytext" id="body">
 						<?php
 							$compendiumtitle = "SELECT * FROM `compendium` WHERE `title` LIKE 'forgeforge'";
 							$titledata = mysqli_query($dbcon, $compendiumtitle) or die('error getting data');
@@ -33,10 +33,10 @@
 							<script>
 							var foundlink = "<?php echo $temp ?>";
 							function replace (querytext){
-								var bodytext = document.getElementById("demo").innerHTML;
+								var bodytext = document.getElementById("body").innerHTML;
 								var url = "<a href=\"onenote:https://d.docs.live.net/acd661a65c5fe18b/Documents/Homebrew%20Campaign/Groups%20and%20Organizations.one#" + querytext + "\">" + querytext + "</a>";
 								var newtext = bodytext.replace(querytext, url)
-								document.getElementById("demo").innerHTML = newtext;
+								document.getElementById("body").innerHTML = newtext;
 							}
 							replace(foundlink);
 
