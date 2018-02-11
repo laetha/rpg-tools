29a7eacb93358f21de4df10615215b1d1b981827
Brian Connor <thaglove10@gmail.com>
2018-02-10 19:02:07 -0500

    compendium.php working

--- tools/compendium.php
+++ tools/compendium.php
@@ -1,6 +1,13 @@
 <?php
 include('../sql-connect.php');
 include('compendium/header.html');
-include('compendium/index.php');
-include('compendium/footer.html');
+$id = "index";
+$disallowed_paths = array('header', 'footer');
+if (!empty($_GET['action'])) {
+  $tmp_action = basename($_GET['action']);
+  if (!in_array($tmp_action, $disallowed_paths) && file_exists("compendium/{$tmp_action}.php"))
+        $id = $tmp_action;
+  }
+  include("compendium/$id.php");
+  include('compendium/footer.html');
 ?>
