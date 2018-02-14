d18311875583594222069e4d3e1ad0fe8ad66d5b
Brian Connor <thaglove10@gmail.com>
2018-02-14 14:19:03 -0500

    Fixed import and added CSS for import page.

--- tools/compendium/import.php
+++ tools/compendium/import.php
@@ -8,17 +8,17 @@ include_once($headpath);
 ?>
   <div class="tocbox col-md-12">
     <div class ="toc body bodytext">
-  <h1 class="pagetitle">Import</h1>
-      <form method="post" action="process.php">
-      <p class="text">Name         <input type="text" name="name" id="name"></p>
+  <h1 class="pagetitle">Add to Compendium</h1>
+      <form method="post" action="process.php" id="import">
+      <p class="text">Name         <input type="text" name="name" id="name" placeholder="Name..."></p>
       <p class="text">Type
-        <select>
-          <option value="settlement" selected>Settlement</option>
+        <select form="import" required="yes">
+          <option value="settlement" selected="selected">Settlement</option>
           <option value="faction">Faction</option>
           <option value="npc">NPC</option>
           <option value="deity">Deity</option>
         </select></p>
-      <p class="text">Body         <textarea type="text" cols="50" rows="10" name="body" id="body"></textarea></p>
+      <p class="text">Body         <textarea type="text" cols="50" rows="10" name="body" id="body" placeholder="Type the body of your content here..."></textarea></p>
       <input type="submit" value="Submit">
     </form>
 </div>
