8cff29653bbfc32e60b02dca0ace86695e8f020e
Brian Connor <thaglove10@gmail.com>
2018-02-18 15:22:57 -0500

    Compendium page formatting, and hyperlinking multiple instances.

--- tools/campaign-log/campaign-log.php
+++ tools/campaign-log/campaign-log.php
@@ -9,23 +9,35 @@
    $headpath .= "/header.php";
    include_once($headpath);
 ?>
+<div class="mainbox col-xs-12">
+  <div class="pagetitle col-xs-12">Campaign Log</div>
 <div id="logsearch">
-    <input class="search" placeholder="Search" />
-    <button class="sort" data-sort="date">
+    <input class="search searchbox" placeholder="Search" />
+    <!--<button class="sort btn btn-primary btn-xs" data-sort="date">
     Sort by Date
     </button>
-    <button class="sort" data-sort="entry">
+    <button class="sort btn btn-info btn-xs" data-sort="entry">
     Sort by Entry
-  </button>
-<div class="list bodytext">
+  </button>-->
+  <div class="padding"></div>
+  <div class="row bodytext tablehead col-xs-12">
+    <div class="date headitem col-sm-3 col-xs-4 col-md-1 col-lg-1">
+      Day
+    </div>
+    <div class="entry headitem col-sm-8 col-xs-8 col-md-10 col-lg-10">
+      Entry
+    </div>
+  </div>
+<div class="list sidebartext" id="thelog">
+
 <?php
-   $logtitle = "SELECT * FROM `campaignlog`";
+   $logtitle = "SELECT * FROM `campaignlog` ORDER BY `date` DESC ";
    $logdata = mysqli_query($dbcon, $logtitle) or die('error getting data');
    while($row =  mysqli_fetch_array($logdata, MYSQLI_ASSOC)) {
-    echo ('<div class="row"><div class="date col-md-1">');
+    echo ('<div class="row"><div class="date col-sm-3 col-xs-2 col-md-1">');
     echo ("Day ");
     echo $row['date'];
-    echo ('</div><div class="entry col-md-10">');
+    echo ('</div><div class="entry col-sm-8 col-xs-8 col-md-10 col-lg-11">');
     echo $row['entry'];
     echo ('</div></div>');
    }
@@ -44,10 +56,11 @@
         <script>
         var foundlink = "<?php echo $temp ?>";
         function replace (querytext){
-          var bodytext = document.getElementById("logsearch").innerHTML;
+          var bodytext = document.getElementById("thelog").innerHTML;
           var url = "<a href=\"/tools/compendium/compendium.php?id=" + querytext + "\">" + querytext + "</a>";
-          var newtext = bodytext.replace(querytext, url)
-          document.getElementById("logsearch").innerHTML = newtext;
+          var regex = new RegExp(querytext, 'g');
+          newtext = bodytext.replace(regex, url);
+          document.getElementById("thelog").innerHTML = newtext;
         }
         replace(foundlink);
 
@@ -60,8 +73,9 @@
         </script>
         <?php
         }
-
-
+?>
+</div>
+<?php
 //Footer
 $footpath = $_SERVER['DOCUMENT_ROOT'];
 $footpath .= "/footer.php";
