938e546a80ae88a3e1f240100e7a6b4c8a777ffe
Brian Connor <thaglove10@gmail.com>
2018-02-19 13:53:20 -0500

    Added buttons for campaign log, DEL works. changes "active" to 0.

--- tools/campaign-log/campaign-log.php
+++ tools/campaign-log/campaign-log.php
@@ -12,36 +12,57 @@
 <div class="mainbox col-xs-12">
   <div class="pagetitle col-xs-12">Campaign Log</div>
 <div id="logsearch">
+
     <input class="search searchbox" placeholder="Search" />
-    <!--<button class="sort btn btn-primary btn-xs" data-sort="date">
-    Sort by Date
-    </button>
-    <button class="sort btn btn-info btn-xs" data-sort="entry">
-    Sort by Entry
-  </button>-->
-  <div class="padding"></div>
+
+
+    <!-- Add to Log -->
+    <p><button class="btn btn-info" id="addbutton">Add to Log</button>
+    <div id="adddiv" style="display:none;">
+      <form method="post" action="logprocess.php" id="logadd">
+        <div class="col-md-1" style="padding-bottom:20px;"><input class="searchbox" style="width:100%;" type="text" name="logdate" id="logdate" placeholder="day #"></div>
+        <div class="col-md-6" style="padding-bottom:20px;"><input class="searchbox" style="width:100%;" type="text" name="logentry" id="logentry" placeholder="Log Entry...."></div>
+        <div class="col-md-1"><input class="btn btn-primary" type="submit" value="Submit"></div>
+    </form>
+  </div>
+  <script>
+  $(document).ready(function(){
+      $("#addbutton").click(function(){
+          $("#adddiv").slideToggle("slow");
+      });
+  });
+  </script>
+  <!--<div class="padding"></div>-->
   <div class="row bodytext tablehead col-xs-12">
-    <div class="date headitem col-sm-3 col-xs-4 col-md-1 col-lg-1">
+    <div class="date headitem col-sm-1 col-xs-1 col-md-1">
       Day
     </div>
-    <div class="entry headitem col-sm-8 col-xs-8 col-md-10 col-lg-10">
+    <div class="logbuttons headitem col-sm-2 col-xs-1 col-md-1 col-lg-1">
+      Edit
+    </div>
+    <div class="entry headitem col-sm-7 col-xs-6 col-md-9 col-lg-9">
       Entry
     </div>
   </div>
 <div class="list sidebartext" id="thelog">
-
+<form action="" method="post">
 <?php
-   $logtitle = "SELECT * FROM `campaignlog` ORDER BY `date` DESC ";
+   $logtitle = "SELECT * FROM `campaignlog` WHERE active=1 ORDER BY `date` DESC ";
    $logdata = mysqli_query($dbcon, $logtitle) or die('error getting data');
    while($row =  mysqli_fetch_array($logdata, MYSQLI_ASSOC)) {
-    echo ('<div class="row"><div class="date col-sm-3 col-xs-2 col-md-1">');
-    echo ("Day ");
+    echo ('<div class="row"><div class="date col-sm-1 col-xs-1 col-md-1">');
+    echo ('Day ');
     echo $row['date'];
-    echo ('</div><div class="entry col-sm-8 col-xs-8 col-md-10 col-lg-11">');
+    echo('</div><div class="logbuttons col-sm-2 col-xs-2 col-md-1 col-lg-1">');
+    echo ('<button type="submit" class="logbtn btn btn-danger btn-sq-xs" name="deleteItem" id="delete-log" value="'.$row['id'].'"><span class="glyphicon glyphicon-remove"></span></button>');
+    echo ('<button type="button" class="logbtn btn btn-info btn-sq-xs" id="edit-log" data-toggle="modal" data-target="#myModal"><span class="glyphicon glyphicon-edit"></span></button>');
+    echo ('</div><div class="entry col-sm-7 col-xs-7 col-md-9 col-lg-10">');
     echo $row['entry'];
-    echo ('</div></div>');
+    echo ('</div>');
+    echo ('</div>');
    }
    ?>
+ </form>
  </div>
 
 </div>
@@ -65,7 +86,15 @@
         replace(foundlink);
 
         var options = {
-          valueNames: [ 'date', 'entry' ]
+          valueNames: [ 'date', 'entry' ],
+          fuzzySearch: {
+            searchClass: "search",
+            location: 0,
+            distance: 1000,
+            threshold: 0.4,
+            multiSearch: true
+  }
+
         };
 
         var userList = new List('logsearch', options);
@@ -73,8 +102,24 @@
         </script>
         <?php
         }
+        // Create variables
+        if(isset($_POST['deleteItem']) and is_numeric($_POST['deleteItem'])){
+        $delid = $_POST['deleteItem'];
+        //Execute the query
+        $sqldelete = "UPDATE campaignlog
+        SET active = 0
+        WHERE id = $delid;";
+                if ($dbcon->query($sqldelete) === TRUE) {
+        					include('delete-modal.php');
+                }
+        				else {
+                    echo "Error: " . $sqldelete . "<br>" . $dbcon->error;
+                }
+}
 ?>
 </div>
+
+
 <?php
 //Footer
 $footpath = $_SERVER['DOCUMENT_ROOT'];
--- tools/campaign-log/delete-modal.php
+++ tools/campaign-log/delete-modal.php
@@ -1,3 +1,15 @@
+<!-- <div class="mainbox col-md-12">
+  <h1 class="pagetitle"><?php echo $name; ?> successfully added!</h1>
+  <div class="menu">
+    <a class="menulink" href="compendium.php?id=<?php echo $name; ?>">><div class="menuitem">
+      <h3>View <?php echo $name; ?> page</h3>
+    </div></a>
+    <a class="menulink" href="/tools/compendium/import.php"><div class="menuitem">
+      <h3>Add another entry</h3>
+    </div></a>
+  </div>
+</div> -->
+
 <?php
 $headpath = $_SERVER['DOCUMENT_ROOT'];
 $headpath .= "/header.php";
@@ -17,10 +29,9 @@ include_once($headpath);
       <div class="modal-content modalstyle bodytext">
 
         <div class="modal-body">
-          <p><?php echo $name; ?> page successfully edited.</p>
+          <p>Entry successfully deleted.</p>
         </div>
         <div class="modal-footer">
-          <a href="compendium.php?id=<?php echo $name; ?>" class="btn btn-info" href>View <strong><?php echo $name; ?></strong> Page</a>
           <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
         </div>
       </div>
