935d5839d30f1d9145f39b4d7b3099253179da8b
Brian Connor <thaglove10@gmail.com>
2018-02-23 19:34:36 -0500

    compendium index page created

--- tools/compendium/toc.php
+++ tools/compendium/toc.php
@@ -5,9 +5,9 @@
 
       <!-- Settlements -->
       <div class="tocitem col-md-3">
-        <h2>ALL</h2>
+        <h2>Backgrounds</h2>
       <?php
-        $sqlcompendium = "SELECT * FROM races, backgrounds";
+        $sqlcompendium = "SELECT title FROM compendium WHERE type LIKE 'background' LIMIT 0,5";
         $compendiumdata = mysqli_query($dbcon, $sqlcompendium) or die('error getting data');
         while($row = mysqli_fetch_array($compendiumdata, MYSQLI_ASSOC)) {
         $entry = $row['title'];
@@ -17,8 +17,83 @@
         echo "<br>";
       }
         ?>
+        <a href="backgrounds.php">View full list...</a>
       </div>
-
+      <div class="tocitem col-md-3">
+        <h2>Feats</h2>
+      <?php
+        $sqlcompendium = "SELECT title FROM compendium WHERE type LIKE 'feat' LIMIT 0,5";
+        $compendiumdata = mysqli_query($dbcon, $sqlcompendium) or die('error getting data');
+        while($row = mysqli_fetch_array($compendiumdata, MYSQLI_ASSOC)) {
+        $entry = $row['title'];
+        echo "<a href=\"compendium.php?id=$entry\">";
+        echo $entry;
+        echo "</a>";
+        echo "<br>";
+      }
+        ?>
+        <a href="feats.php">View full list...</a>
+      </div>
+      <div class="tocitem col-md-3">
+        <h2>Items</h2>
+      <?php
+        $sqlcompendium = "SELECT title FROM compendium WHERE type LIKE 'item' LIMIT 0,5";
+        $compendiumdata = mysqli_query($dbcon, $sqlcompendium) or die('error getting data');
+        while($row = mysqli_fetch_array($compendiumdata, MYSQLI_ASSOC)) {
+        $entry = $row['title'];
+        echo "<a href=\"compendium.php?id=$entry\">";
+        echo $entry;
+        echo "</a>";
+        echo "<br>";
+      }
+        ?>
+        <a href="items.php">View full list...</a>
+      </div>
+      <div class="tocitem col-md-3">
+        <h2>Monsters</h2>
+      <?php
+        $sqlcompendium = "SELECT title FROM compendium WHERE type LIKE 'monster' LIMIT 0,5";
+        $compendiumdata = mysqli_query($dbcon, $sqlcompendium) or die('error getting data');
+        while($row = mysqli_fetch_array($compendiumdata, MYSQLI_ASSOC)) {
+        $entry = $row['title'];
+        echo "<a href=\"compendium.php?id=$entry\">";
+        echo $entry;
+        echo "</a>";
+        echo "<br>";
+      }
+        ?>
+        <a href="monsters.php">View full list...</a>
+      </div>
+      <div class="tocitem col-md-3">
+        <h2>Races</h2>
+      <?php
+        $sqlcompendium = "SELECT title FROM compendium WHERE type LIKE 'race' LIMIT 0,5";
+        $compendiumdata = mysqli_query($dbcon, $sqlcompendium) or die('error getting data');
+        while($row = mysqli_fetch_array($compendiumdata, MYSQLI_ASSOC)) {
+        $entry = $row['title'];
+        echo "<a href=\"compendium.php?id=$entry\">";
+        echo $entry;
+        echo "</a>";
+        echo "<br>";
+      }
+        ?>
+        <a href="races.php">View full list...</a>
+      </div>
+      <div class="tocitem col-md-3">
+        <h2>Spells</h2>
+      <?php
+        $sqlcompendium = "SELECT title FROM compendium WHERE type LIKE 'spell' LIMIT 0,5";
+        $compendiumdata = mysqli_query($dbcon, $sqlcompendium) or die('error getting data');
+        while($row = mysqli_fetch_array($compendiumdata, MYSQLI_ASSOC)) {
+        $entry = $row['title'];
+        echo "<a href=\"compendium.php?id=$entry\">";
+        echo $entry;
+        echo "</a>";
+        echo "<br>";
+      }
+        ?>
+        <a href="spells.php">View full list...</a>
+      </div>
 
       </div>
   </div>
