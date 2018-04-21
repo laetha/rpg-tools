2e929f669f33e153468ecec6b53add443bf73d9e
Brian Connor <thaglove10@gmail.com>
2018-04-21 18:55:00 -0400

    Added calendar

---
+++ tools/world/calendar.php
@@ -0,0 +1,25 @@
+<?php
+  //SQL Connect
+   $sqlpath = $_SERVER['DOCUMENT_ROOT'];
+   $sqlpath .= "/sql-connect.php";
+   include_once($sqlpath);
+   $pgtitle = $_GET['id'].' - ';
+   if(empty($_GET['id'])) {
+     $pgtitle = 'Calendar - ';
+   }
+   //Header
+   $headpath = $_SERVER['DOCUMENT_ROOT'];
+   $headpath .= "/header.php";
+   include_once($headpath);
+
+?>
+<div class="mainbox">
+
+<img src="/assets/images/calendar.png" style="width:95%; max-width: 1000px; margin-top: 20px;"/>
+</div>
+<?php
+//Footer
+$footpath = $_SERVER['DOCUMENT_ROOT'];
+$footpath .= "/footer.php";
+include_once($footpath);
+?>
