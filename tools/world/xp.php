5529ef0a0cc837c42f2ddc065143f11639cd640b
Brian Connor <thaglove10@gmail.com>
2018-05-24 10:26:06 -0400

    Added xp progression chart to XP page

--- tools/world/xp.php
+++ tools/world/xp.php
@@ -81,6 +81,129 @@
     </table>
      </div>
 
+     <div class="sidebartext col-md-6">
+       <p style="margin-top:40px;">XP Progression:</p>
+       <table id="XP" class="table table-condensed table-striped table-responsive dt-responsive" cellspacing="0" width="100%">
+               <thead class="thead-dark">
+                   <tr>
+                       <th scope="col">XP</th>
+                       <th scope="col">Level</th>
+                       <th scope="col">Proficiency</th>
+
+                   </tr>
+               </thead>
+               <tfoot>
+                   <tr>
+                     <th scope="col">XP</th>
+                     <th scope="col">Level</th>
+                     <th scope="col">Proficiency</th>
+                   </tr>
+               </tfoot>
+               <tbody>
+                <tr>
+                  <td>0</td>
+                  <td>1</td>
+                  <td>+2</td>
+                </tr>
+                <tr>
+                  <td>300</td>
+                  <td>2</td>
+                  <td>+2</td>
+                </tr>
+                <tr>
+                  <td>900</td>
+                  <td>3</td>
+                  <td>+2</td>
+                </tr>
+                <tr>
+                  <td>2700</td>
+                  <td>4</td>
+                  <td>+2</td>
+                </tr>
+                <tr>
+                  <td>6500</td>
+                  <td>5</td>
+                  <td>+3</td>
+                </tr>
+                <tr>
+                  <td>14000</td>
+                  <td>6</td>
+                  <td>+3</td>
+                </tr>
+                <tr>
+                  <td>23000</td>
+                  <td>7</td>
+                  <td>+3</td>
+                </tr>
+                <tr>
+                  <td>34000</td>
+                  <td>8</td>
+                  <td>+3</td>
+                </tr>
+                <tr>
+                  <td>48000</td>
+                  <td>9</td>
+                  <td>+4</td>
+                </tr>
+                <tr>
+                  <td>64000</td>
+                  <td>10</td>
+                  <td>+4</td>
+                </tr>
+                <tr>
+                  <td>85000</td>
+                  <td>11</td>
+                  <td>+4</td>
+                </tr>
+                <tr>
+                  <td>100000</td>
+                  <td>12</td>
+                  <td>+4</td>
+                </tr>
+                <tr>
+                  <td>120000</td>
+                  <td>13</td>
+                  <td>+5</td>
+                </tr>
+                <tr>
+                  <td>140000</td>
+                  <td>14</td>
+                  <td>+5</td>
+                </tr>
+                <tr>
+                  <td>165000</td>
+                  <td>15</td>
+                  <td>+5</td>
+                </tr>
+                <tr>
+                  <td>195000</td>
+                  <td>16</td>
+                  <td>+5</td>
+                </tr>
+                <tr>
+                  <td>225000</td>
+                  <td>17</td>
+                  <td>+6</td>
+                </tr>
+                <tr>
+                  <td>265000</td>
+                  <td>18</td>
+                  <td>+6</td>
+                </tr>
+                <tr>
+                  <td>305000</td>
+                  <td>19</td>
+                  <td>+6</td>
+                </tr>
+                <tr>
+                  <td>355000</td>
+                  <td>20</td>
+                  <td>+6</td>
+                </tr>
+    </tbody>
+    </table>
+     </div>
+
          </div>
 
 </div>
