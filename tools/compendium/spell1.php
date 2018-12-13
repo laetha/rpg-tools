779e698f7b2d727f35b4d4a04aa24bcd4643c6f3
Brian Connor <thaglove10@gmail.com>
2018-12-13 14:56:55 -0500

    still working on spells page

--- tools/compendium/spell1.php
+++ tools/compendium/spell1.php
@@ -21,10 +21,31 @@
    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap.min.css">
    <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.1/css/responsive.bootstrap.min.css">
    <div class="mainbox col-lg-10 col-xs-12 col-lg-offset-1">
+     <?php
+     $id = "index";
+     $disallowed_paths = array('header', 'footer');
+     if (!empty($_GET['id'])) {
+       $tmp_action = basename($_GET['id']);
+       if (!in_array($tmp_action, $disallowed_paths) /*&& file_exists("world/{$tmp_action}.php")*/)
+             $id = $tmp_action;
 
+   $stripid = str_replace("'", "", $id);
+   $stripid = stripslashes($stripid);
+   $id = addslashes($id);
+   $worldtitle = "SELECT * FROM `characters` WHERE `title` LIKE '$id'";
+   $titledata = mysqli_query($dbcon, $worldtitle) or die('error getting data');
+   while($row =  mysqli_fetch_array($titledata, MYSQLI_ASSOC)) {
+     if ($row['user'] !== $loguser && $loguser !== 'tarfuin'){
+       header("Location: /tools/users/login.php");
+       die();
+     }
+     $allspells = $row['spells'];
+     $spellsarray = explode(',', $allspells);
+     $spellsarray = join("','",$spellsarray);
+       ?>
      <!-- Page Header -->
      <div class="col-md-12">
-     <div class="pagetitle" id="pgtitle">My Spells</div>
+     <div class="pagetitle" id="pgtitle"><?php echo ucwords($row['title']); ?>'s' Spells</div>
    </div>
      <div class="body sidebartext col-xs-12" id="body">
        <div class="table-responsive">
@@ -36,6 +57,7 @@
                    <th scope="col">Casting Time</th>
                    <th scope="col">Duration</th>
                    <th scope="col">Range</th>
+                   <th scope="col">Attack/Save</th>
                    <th scope="col" class="none">Description</th>
                </tr>
            </thead>
@@ -46,13 +68,14 @@
                  <th scope="col">Casting Time</th>
                  <th scope="col">Duration</th>
                  <th scope="col">Range</th>
+                 <th scope="col">Attack/Save</th>
                  <th scope="col">Description</th>
                </tr>
            </tfoot>
            <tbody>
              <?php
                $a = 1;
-               $sqlcompendium = "SELECT * FROM compendium WHERE type LIKE 'spell' AND title NOT LIKE '%*' AND title NOT LIKE '%(Ritual Only)' AND title NOT LIKE '%invocation%'";
+               $sqlcompendium = "SELECT * FROM compendium WHERE type LIKE 'spell' AND title NOT LIKE '%*' AND title NOT LIKE '%(Ritual Only)' AND title NOT LIKE '%invocation%' AND title IN ('$spellsarray')";
                $compendiumdata = mysqli_query($dbcon, $sqlcompendium) or die('error getting data');
                while($row = mysqli_fetch_array($compendiumdata, MYSQLI_ASSOC)) {
                echo ('<tr><td>');
@@ -64,6 +87,37 @@
                echo ('<td>'.$row['spellTime'].'</td>');
                echo ('<td>'.$row['spellDuration'].'</td>');
                echo ('<td>'.$row['spellRange'].'</td>');
+               if (strpos($row['text'], 'Strength saving throw') !== false){
+                 echo ('<td>STR Save</td>');
+
+               }
+               else if (strpos($row['text'], 'Dexterity saving throw') !== false){
+                 echo ('<td>DEX Save</td>');
+
+               }
+               else if (strpos($row['text'], 'Constitution saving throw') !== false){
+                 echo ('<td>CON Save</td>');
+
+               }
+               else if (strpos($row['text'], 'Intelligence saving throw') !== false){
+                 echo ('<td>INT Save</td>');
+
+               }
+               else if (strpos($row['text'], 'Wisdom saving throw') !== false){
+                 echo ('<td>WIS Save</td>');
+
+               }
+               else if (strpos($row['text'], 'Charisma saving throw') !== false){
+                 echo ('<td>CHA Save</td>');
+
+               }
+               else if (strpos($row['text'], 'spell attack') !== false){
+                 echo ('<td>Spell Attack</td>');
+
+               }
+               else {
+                 echo ('<td>-</td>');
+               }
                $spelldeet = substr($row['text'], 0, strpos($row['text'], "Source:"));
                $spelldeet = rtrim($spelldeet);
                echo ('<td id="spell'.$a.'">'.nl2br($spelldeet).'</td>');
@@ -76,7 +130,10 @@
 </tbody>
 </table>
 
-
+<?php
+}
+}
+ ?>
 <script>
 $(document).ready(function() {
     // Setup - add a text input to each footer cell
@@ -89,7 +146,8 @@ $(document).ready(function() {
                 display: $.fn.dataTable.Responsive.display.childRowImmediate,
                 type: ''
             }
-        }
+        },
+         "order": [[ 1, "asc" ]]
 
     } );
 
