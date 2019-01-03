3af124feec90f4133ecb807ac883ff7ed624c8cd
Brian Connor <thaglove10@gmail.com>
2019-01-03 11:24:45 -0500

    testing user notes

--- tools/users/charindex.php
+++ tools/users/charindex.php
@@ -111,6 +111,7 @@ function editSheet() {
       die();
     }
    //echo $row['title'];
+   $cahruser = $row['user'];
    $title = $row['title'];
    $charID = $row['id'];
    $fullclass = $row['class1'];
@@ -4229,7 +4230,7 @@ echo ('<div class="featureDetails collapse" id="'.$featuretitlens.'show" name="'
     <table id="myNotes" class="table table-condensed table-striped table-responsive dt-responsive halftable" cellspacing="0" width="50%">
       <?php
       $notecount = 1;
-      $notestitle = "SELECT * FROM `notes` WHERE `user` LIKE '$loguser'";
+      $notestitle = "SELECT * FROM `notes` WHERE `user` LIKE '$charuser'";
       $notesdata = mysqli_query($dbcon, $notestitle) or die('error getting data');
       while($notesrow =  mysqli_fetch_array($notesdata, MYSQLI_ASSOC)) {
           echo ('<tr><td><a class="featureName" data-toggle="collapse" href="#'.$notecount.'note">'.$notesrow['title'].'</a></td></tr>');
