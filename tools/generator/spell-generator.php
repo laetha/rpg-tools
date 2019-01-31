d58984e242d3a08c15147ef68a2af28197f781a0
Brian Connor <thaglove10@gmail.com>
2019-01-31 13:07:35 -0500

    added cantrips to spell generator

--- tools/generator/spell-generator.php
+++ tools/generator/spell-generator.php
@@ -34,6 +34,7 @@
 
          <select form="import" required="yes" name="level" id="level">
            <option value="all">All Levels</option>
+           <option value="0">Cantrip</option>
            <option value="1">1</option>
            <option value="2">2</option>
            <option value="3">3</option>
