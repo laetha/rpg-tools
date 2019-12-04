af08eb54b736b9c37bf3661462b4e6ad9e62685a
Brian Connor <thaglove10@gmail.com>
2019-12-03 22:20:59 -0500

    added items to dbimport

--- tools/world/dbprocess.php
+++ tools/world/dbprocess.php
@@ -18,6 +18,13 @@ $raceAbilitytemp=$_POST['raceAbility'];
 $raceSpellAbilitytemp=$_POST['raceSpellAbility'];
 $raceProficiencytemp=$_POST['raceProficiency'];
 $raceTraitstemp=$_POST['raceTraits'];
+$itemTexttemp=$_POST['itemText'];
+$itemTypetemp=$_POST['itemType'];
+$itemStocktemp=$_POST['itemStock'];
+$itemDetailtemp=$_POST['itemDetail'];
+$itemValuetemp=$_POST['itemValue'];
+$itemMagictemp=$_POST['itemMagic'];
+
 
 $name=htmlentities(trim(addslashes($nametemp)));
 $type=htmlentities(trim(addslashes($typetemp)));
@@ -27,11 +34,17 @@ $raceAbility=htmlentities(trim(addslashes($raceAbilitytemp)));
 $raceSpellAbility=htmlentities(trim(addslashes($raceSpellAbilitytemp)));
 $raceProficiency=htmlentities(trim(addslashes($raceProficiencytemp)));
 $raceTraits=htmlentities(trim(addslashes($raceTraitstemp)));
+$itemText=htmlentities(trim(addslashes($itemTexttemp)));
+$itemType=htmlentities(trim(addslashes($itemTypetemp)));
+$itemStock=htmlentities(trim(addslashes($itemStocktemp)));
+$itemDetail=htmlentities(trim(addslashes($itemDetailtemp)));
+$itemValue=htmlentities(trim(addslashes($itemValuetemp)));
+$itemMagic=htmlentities(trim(addslashes($itemMagictemp)));
 
 
 //Execute the query
-$sql = "INSERT INTO compendium(title,type,raceSize,raceSpeed,raceAbility,raceSpellAbility,raceProficiency,raceTraits)
-				VALUES('$name','$type','$raceSize','$raceSpeed','$raceAbility','$raceSpellAbility','$raceProficiency','$raceTraits')";
+$sql = "INSERT INTO compendium(title,type,raceSize,raceSpeed,raceAbility,raceSpellAbility,raceProficiency,raceTraits,text,itemType,itemStock,itemDetail,itemValue,itemMagic)
+				VALUES('$name','$type','$raceSize','$raceSpeed','$raceAbility','$raceSpellAbility','$raceProficiency','$raceTraits','$itemText','$itemType','$itemStock','$itemDetail','$itemValue','$itemMagic')";
 
         if ($dbcon->query($sql) === TRUE) {
 					include('success.php');
